66c4a34fa0d97debf2d127c5f0e56bdcc91de4d3
Paul Adam <pavlosadam@gmail.com>
2023-10-12 21:54:58 +0300

    Bump kotlin version 1.8.0 -> 1.9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version "1.9.0"
     id("org.jetbrains.compose") version "1.3.1"
 }
 
